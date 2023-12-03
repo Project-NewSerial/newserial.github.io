4135f55d55ab5a8e2bb0b73c7a511dcdbb4ed923
jeongeun <wjddms9921@gmail.com>
2023-12-03 11:17:43 +0900

    DESIGN : Login 페이지 퍼블리싱

--- newserial/src/pages/Login/index.tsx
+++ newserial/src/pages/Login/index.tsx
@@ -1,5 +1,37 @@
+import {
+  Container,
+  Logo,
+  InputContent,
+  InputBox,
+  InputText,
+  Input,
+  Button,
+  BottomText,
+} from "./styles";
+
 const Login = () => {
-  return <div>로그인페이지입니다.</div>;
+  return (
+    <Container>
+      <Logo>NEWSERIAL</Logo>
+      <InputContent>
+        <InputBox>
+          <InputText>이메일</InputText>
+          <Input />
+        </InputBox>
+        <InputBox>
+          <InputText>비밀번호</InputText>
+          <Input type="password" />
+        </InputBox>
+      </InputContent>
+      <Button>로그인</Button>
+      <BottomText>
+        <div>회원가입</div>
+        <div>&nbsp; | &nbsp;</div>
+        <div>비밀번호 재설정</div>
+      </BottomText>
+      <img src="/assets/images/btnD_완성형.png" width={150}/>
+    </Container>
+  );
 };
 
 export default Login;
