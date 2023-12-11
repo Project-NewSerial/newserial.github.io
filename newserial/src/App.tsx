a0950eef41b70b96a8fd0122182a38735b17ed05
jeongeun <wjddms9921@gmail.com>
2023-12-11 23:50:52 +0900

    ADD : react-query 추가

--- newserial/src/App.tsx
+++ newserial/src/App.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Login from "./pages/Login/index";
 import Signup from "./pages/Signup/index";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </QueryClientProvider>
   );
 }
 
