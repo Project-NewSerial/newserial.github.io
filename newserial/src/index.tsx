7bf1d1db0dd2d5ac6cdc0865723cb1c1758e7be2
VanesaK <minitea@sookmyung.ac.kr>
2024-03-14 16:53:19 +0900

    FIX: 로그인, 모달 develop과 맞추기

--- newserial/src/index.tsx
+++ newserial/src/index.tsx
@@ -14,7 +14,6 @@ import store from "./redux/store";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-
 root.render(
   <React.StrictMode>
     <Provider store={store}>
