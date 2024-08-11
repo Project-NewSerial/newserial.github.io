bbc7098646be7b142c8bbd659cc58fa37a379d31
jeongeun <wjddms9921@gmail.com>
2024-08-11 11:09:55 +0900

    refactor: 회원가입 api, react-query 분리

--- newserial/src/api/auth.ts
+++ newserial/src/api/auth.ts
@@ -5,6 +5,22 @@ export interface LoginParams {
   password: string;
 }
 
+export interface SendMailParams {
+  email: string;
+}
+
+export interface CheckNumberParams {
+  email: string;
+  code: string;
+}
+
+export interface SignupParams {
+  email: string;
+  code: string;
+}
+
+//로그인
+
 export const login = async (params: LoginParams) =>
   await api.post(
     `/login`,
@@ -20,3 +36,20 @@ export const naverLogin = async () => {
   const url = `${process.env.REACT_APP_API}/oauth2/authorization/naver`;
   return (window.location.href = new URL(url, window.location.origin).href);
 };
+
+//회원가입
+
+export const sendMail = async (params: SendMailParams) =>
+  await api.post(`/email`, {
+    params,
+  });
+
+export const checkNumber = async (params: CheckNumberParams) =>
+  await api.post(`/email-number`, {
+    params,
+  });
+
+export const signup = async (params: SignupParams) =>
+  await api.post(`/members`, {
+    params,
+  });
--- newserial/src/state/react-query/auth.ts
+++ newserial/src/state/react-query/auth.ts
@@ -1,5 +1,16 @@
 import { useMutation } from "@tanstack/react-query";
-import { login, LoginParams, naverLogin } from "../../api/auth";
+import {
+  checkNumber,
+  CheckNumberParams,
+  login,
+  LoginParams,
+  naverLogin,
+  sendMail,
+  SendMailParams,
+  signup,
+  SignupParams,
+} from "../../api/auth";
+import { useNavigate } from "react-router-dom";
 
 export const useMutationLogin = (params: LoginParams) =>
   useMutation({
@@ -12,3 +23,25 @@ export const useMutationNaverLogin = () =>
     mutationFn: naverLogin,
     onError: () => alert("로그인에 실패했습니다."),
   });
+
+export const useMutationSeneMail = (params: SendMailParams) =>
+  useMutation({
+    mutationFn: () => sendMail(params),
+  });
+
+export const useMutationCheckNumber = (params: CheckNumberParams) =>
+  useMutation({
+    mutationFn: () => checkNumber(params),
+  });
+
+export const useMutationSignup = (params: SignupParams) => {
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: () => signup(params),
+    onSuccess: (res) => {
+      alert(res.data.message);
+      navigate("/login");
+    },
+  });
+};
